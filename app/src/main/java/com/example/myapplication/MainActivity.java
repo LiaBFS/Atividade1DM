b2510570df3a1e3be025a27d7fd7992e20f47a49
Lia <lia.bfs@aluno.ifsc.edu.br>
2025-08-15 10:49:40 -0300

    Contador

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int contador = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tv);
+        textView.setText(Integer.toString(contador));
+
+        textView.setOnClickListener(v -> {
+            contador++;
+            textView.setText(Integer.toString(contador));
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
